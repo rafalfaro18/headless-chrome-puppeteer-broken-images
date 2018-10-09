ed362a1cc6d741d667cda931599051ad3d4c4b91
Rafael Alfaro <rafaela@thehangar.cr>
2018-10-09 16:49:17 -0600

    adds dotenv and logs in

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+require('dotenv').config()
 const puppeteer = require('puppeteer');
 
 (async() => {
   const browser = await puppeteer.launch();
-  console.log(await browser.version());
+  const page = await browser.newPage();
+  try {
+  await page.authenticate({username:process.env.HT_USER, password:process.env.HT_PASS});
+  await page.goto(process.env.SITE_URL, {waitUntil: 'networkidle2'});
+  await page.pdf({path: 'page.pdf', format: 'A4'});
   await browser.close();
+  }
+  catch (e){
+    console.error(e);
+  }
 })();
