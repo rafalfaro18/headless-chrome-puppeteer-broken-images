82beac2aeba0522ea41e188c7492ab40c5d1dca1
Rafael Alfaro <rafaela@thehangar.cr>
2018-10-09 22:29:40 -0600

    makes a function getImages that receives site url

--- index.js
+++ index.js
@@ -2,20 +2,8 @@ require('dotenv').config()
 const puppeteer = require('puppeteer');
 var fs = require('fs');
 
-(async() => {
-  fs.unlink('broken_images.txt', function (err) {
-    if (err) throw err;
-    console.log('File deleted!');
-  });
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const username = process.env.HT_USER;
-  const password = process.env.HT_PASS;
-  try {
-  if (username && password) {
-    await page.authenticate({username, password});
-  }
-  await page.goto(process.env.SITE_URL, {waitUntil: 'networkidle2'});
+async function getImages(page, url , index=0){
+  await page.goto(url, {waitUntil: 'networkidle2'});
   const images_links = await page.$$eval('img', image => image.filter(image => image.naturalWidth == 0 || image.readyState == 'uninitialized').map(img => img.src));
   if (images_links) {
     images_links.forEach((image) => {
@@ -26,7 +14,24 @@ var fs = require('fs');
       });
     });
   }
-  await page.screenshot({path: 'page.png', fullPage: true });
+  await page.screenshot({path: `page-${index}.png`, fullPage: true });
+}
+
+(async() => {
+  fs.writeFile('broken_images.txt', '', function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
+  });
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const username = process.env.HT_USER;
+  const password = process.env.HT_PASS;
+  const same_site_links = process.env.SAME_SITE_LINKS.split(',');
+  try {
+  if (username && password) {
+    await page.authenticate({username, password});
+  }
+  await getImages(page, process.env.SITE_URL, 0);
   await browser.close();
   }
   catch (e){
