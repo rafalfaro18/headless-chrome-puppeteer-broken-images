8713ac488f1f93df06f6a7035f4aea4c6c710241
Rafael Alfaro <rafaela@thehangar.cr>
2018-10-09 22:53:18 -0600

    iterates over all links

--- index.js
+++ index.js
@@ -2,19 +2,39 @@ require('dotenv').config()
 const puppeteer = require('puppeteer');
 var fs = require('fs');
 
-async function getImages(page, url , index=0){
-  await page.goto(url, {waitUntil: 'networkidle2'});
-  const images_links = await page.$$eval('img', image => image.filter(image => image.naturalWidth == 0 || image.readyState == 'uninitialized').map(img => img.src));
-  if (images_links) {
-    images_links.forEach((image) => {
-      console.log(image);
-      fs.appendFile('broken_images.txt', image+'\n', function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-    });
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
   }
-  await page.screenshot({path: `page-${index}.png`, fullPage: true });
+}
+
+async function getImages(url , index=0){
+  let username = process.env.HT_USER;
+  let password = process.env.HT_PASS;
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
+  try {
+    if (username && password) {
+      await page.authenticate({username, password});
+    }
+
+    await page.goto(url, {waitUntil: 'networkidle2'});
+    const images_links = await page.$$eval('img', image => image.filter(image => image.naturalWidth == 0 || image.readyState == 'uninitialized').map(img => img.src));
+    if (images_links) {
+      images_links.forEach((image) => {
+        console.log(image);
+        fs.appendFile('broken_images.txt', image+'\n', function (err) {
+          if (err) throw err;
+          console.log('Saved!');
+        });
+      });
+    }
+    await page.screenshot({path: `page-${index}.png`, fullPage: true });
+    await browser.close();
+  }
+  catch (e){
+    console.error(e);
+  }
 }
 
 (async() => {
@@ -22,17 +42,18 @@ async function getImages(page, url , index=0){
     if (err) throw err;
     console.log('Replaced!');
   });
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const username = process.env.HT_USER;
-  const password = process.env.HT_PASS;
+
   const same_site_links = process.env.SAME_SITE_LINKS.split(',');
   try {
-  if (username && password) {
-    await page.authenticate({username, password});
-  }
-  await getImages(page, process.env.SITE_URL, 0);
-  await browser.close();
+
+  await asyncForEach(same_site_links, async (link, index) => {
+    try {
+      await getImages(link, index);
+    }
+    catch (e){
+      console.error(e);
+    }
+  });
   }
   catch (e){
     console.error(e);
