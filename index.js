7f790db69207d2de6b714b1b8a1b046684a0a6de
Rafael Alfaro <rafaela@thehangar.cr>
2018-10-09 20:29:16 -0600

    takes screenshot instead of pdf

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const puppeteer = require('puppeteer');
   try {
   await page.authenticate({username:process.env.HT_USER, password:process.env.HT_PASS});
   await page.goto(process.env.SITE_URL, {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'page.pdf', format: 'A4'});
+  await page.screenshot({path: 'page.png', fullPage: true });
   await browser.close();
   }
   catch (e){
