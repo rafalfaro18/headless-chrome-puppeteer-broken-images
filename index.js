2e7b85f59dde6a4639f012524de88d4a0cfa9d44
Rafael Alfaro <rafaela@thehangar.cr>
2018-10-09 21:41:57 -0600

    only await login if theres username and password

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const puppeteer = require('puppeteer');
 (async() => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  const username = process.env.HT_USER;
+  const password = process.env.HT_PASS;
   try {
-  await page.authenticate({username:process.env.HT_USER, password:process.env.HT_PASS});
+  if (username && password) {
+    await page.authenticate({username, password});
+  }
   await page.goto(process.env.SITE_URL, {waitUntil: 'networkidle2'});
   const images_links = await page.$$eval('img', image => image.filter(image => image.naturalWidth == 0 || image.readyState == 'uninitialized').map(img => img.src));
   if (images_links) {
